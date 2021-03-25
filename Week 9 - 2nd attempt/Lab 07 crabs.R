bb0cdf679e777444282a188e14be7f5d2e645ca7
tomhanna-uh <tlhanna@uh.edu>
2021-03-24 23:46:49 -0500

    Corrected labs 7 and 8

---
+++ Week 9 - 2nd attempt/Lab 07 crabs.R
@@ -0,0 +1,51 @@
+rm(list=ls())
+
+crab <- read.table("C:/crab.txt")
+colnames(crab)=c("Obs","C","S","W","Wt","Sa")
+crab=crab[,-1] #removes "Obs" column
+
+attach(crab)
+plot(W, Sa, pch=19)
+abline(lm(Sa ~ W))
+
+lmodel=lm(Sa ~ W); summary(lmodel)
+# lmodel=glm(Sa ~ 1+ W, family=gaussian); summary(lmodel)
+
+pmodel=glm(Sa ~ 1+ W, family=poisson(link=log))
+summary(pmodel)
+
+print=data.frame(crab,pred=pmodel$fitted)
+print
+fits=lm(Sa~pred, print); summary(fits)$r.squared
+
+pmodel$linear.predictors
+exp(pmodel$linear.predictors)
+
+nmodel=glm(crab$Sa~1, family=poisson(link=log))
+summary(nmodel)
+nmodel$fitted
+
+pmodel$linear.predictors[crab$W==25]
+pmodel$linear.predictors[crab$W==26]
+pmodel$fitted[crab$W==25]
+pmodel$fitted[crab$W==26]
+exp(-pmodel$fitted[crab$W==25])
+exp(-pmodel$fitted[crab$W==26])
+exp(-pmodel$fitted[crab$W==25])*pmodel$fitted[crab$W==25]
+exp(-pmodel$fitted[crab$W==26])*pmodel$fitted[crab$W==26]
+
+pmodel$fitted[crab$W==26]/pmodel$fitted[crab$W==25]
+pmodel$fitted[2]/pmodel$fitted[6]
+
+confint(pmodel,2)
+exp(confint(pmodel,2))-1
+# install.packages("sandwich")
+library(sandwich)
+robvar<-vcovHC(pmodel, type="HC0")
+robse <- sqrt(diag(robvar))
+r.est <- cbind(Estimate= coef(pmodel), "Robust SE" = robse, "Pr(>|z|)" = 2*pnorm(abs(coef(pmodel)/robse), lower.tail=FALSE),
+               LL = coef(pmodel) - 1.96*robse, UL = coef(pmodel) + 1.96*robse)
+options(scipen=999); round(r.est, digits=5)
+
+library(car)
+robvar<-hccm(pmodel)
---
+++ Week 9 - 2nd attempt/Lab 07 health.R
@@ -0,0 +1,47 @@
+library(foreign); library(car)
+health <- read.dta("C:/OECD_health.dta")
+subset <- subset(health, country != "South Africa")
+
+plot(life_expect ~ health_expend, subset, pch=19)
+abline(lm(subset$life_expect ~ subset$health_expend))
+
+par(mfrow=c(1,2))
+hist(subset$life_expect, freq = FALSE)
+qqnorm(subset$life_expect); qqline(subset$life_expect)
+hist(log(subset$life_expect), freq = FALSE)
+qqnorm(log(subset$life_expect)); qqline(log(subset$life_expect))
+hist(subset$health_expend, freq = FALSE)
+qqnorm(subset$health_expend); qqline(subset$health_expend)
+par(mfrow=c(1,1))
+
+plot(log(life_expect) ~ log(health_expend), subset, pch=19)
+abline(lm(log(subset$life_expect) ~ log(subset$health_expend)))
+
+subset$health.1k = subset$health_expend/1000
+linlin <- lm(life_expect ~ health.1k, subset); summary(linlin)
+plot(subset$health.1k, linlin$residuals, pch=19); abline(h=0, col="red") #plot(linlin)
+
+linlog <- lm(life_expect ~ log(health.1k), subset); summary(linlog)
+plot(subset$health.1k, linlog$residuals, pch=19); abline(h=0, col="red") #plot(levlog)
+
+uhat <- linlog$residuals; uhatsq <- uhat^2
+x <- subset$health.1k; xsq <- (subset$health.1k)^2
+white <- lm(uhatsq ~ x + xsq); summary(white)
+
+loglog <- lm(log(life_expect) ~ log(health.1k), subset); summary(loglog)
+plot(subset$health.1k, loglog$residuals, pch=19); abline(h=0, col="red") #plot(loglog)
+
+expu <- exp(loglog$residuals)
+alph <- mean(expu)
+subset$lifehat <- alph*exp(fitted(loglog))
+logcheck <- lm(life_expect~lifehat, subset); summary(logcheck)
+
+uhatlog <- loglog$residuals; uhatlogsq <- uhatlog^2
+whitelog <- lm(uhatlogsq ~ x + xsq); summary(whitelog)
+
+plot(subset$health.1k, subset$life_expect, pch=21, cex=.75)
+lines(sort(subset$health.1k), fitted(logcheck)[order(subset$health.1k)], col='red', type='b', pch=19)
+abline(lm(life_expect~health.1k, subset), col='purple', lwd=3)
+
+rm(health); rm(subset); rm(alph); rm(expu)
+rm(linlin); rm(linlog); rm(loglog); rm(logcheck)
