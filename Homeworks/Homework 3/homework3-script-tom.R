4f4f9b617aa1cbe74dcac18a1750614ae456323e
tomhanna-uh <tlhanna@uh.edu>
2021-04-10 14:38:21 -0500

    misc

--- Homeworks/Homework 3/homework3-script-tom.R
+++ Homeworks/Homework 3/homework3-script-tom.R
@@ -9,7 +9,9 @@ names(charity)
 q1 <- lm(gift ~ mailsyear, data = charity)
 summary(q1)
 
-#Problem 1 - Test B1 = 1
+#Problem 1 - Test B1 = 1 (parts 2 and 3)
+
+confint(q1)
 
 #Just the regression problem 2
 q2 <- lm(gift ~ mailsyear + avggift + propresp, data = charity)
@@ -17,20 +19,46 @@ summary(q2)
 
 #Problem 2 - Test B1 = 1
 
-#Problem 2 - F Test avggift  and propresp  B1 and B2 = 0
+confint(q2)
 
+#Problem 2 - F Test avggift  and propresp  B1 and B2 = 0
+library(car)
+linearHypothesis(q2,c("avggift=0","propresp=0"))
 
 #Problem 3 - Heteroskedasticity
 
 #Plot residuals against fitted values
 
+charity$res <- q2$residuals
+par(mfrow=c(2,2))
+plot(q2,which=1)
+plot(charity$mailsyear,charity$res,pch=19,col="purple")
+plot(charity$avggift,charity$res,pch=19,col="red")
+plot(charity$propresp,charity$res,pch=19,col="blue")
+par(mfrow=c(1,1))
+
 #Test for heteroskedasticity
+
+
+#I use the white test script in the "other scripts" directory
+source(here("other scripts","white-test.r"))
+white.test(q2)
+
 
 #Re-estimate q2 using FGLS
 
+charity$sqr <- (charity$res)^2
+charity$sqgift <- (charity$gift)^2
+fgls <- lm(charity$sqr ~ charity$gift + charity$sqgift)
+charity$wsq <- predict(fgls)
+wls <- lm(gift ~ mailsyear + avggift + propresp, data = charity, weights = 1/wsq)
+summary(wls)
 
 #Problem 4 - Reestimate q2 with robust standard errors
-
+library(sandwich)
+robse<-vcovHC(q2)
+sqrt(robse[2,2]); sqrt(robse[3,3]); sqrt(robse[4,4])
+coeftest(q2,robse)
 
 ##Part 2
 library(haven)
@@ -52,14 +80,36 @@ summary(q7)
 
 
 #q 7.5
+q75 <- lm(wage ~ female, data =  beauty)
+summary(q75)
 
 #q8 Heteroskedasticity
 
 #plot residuals against regressors and/or fitted values
 
+beauty$res <- q7$residuals
+par(mfrow=c(3,2))
+plot(beauty$looks,beauty$res,pch=19,col="purple")
+plot(beauty$educ,beauty$res,pch=19,col="red")
+plot(beauty$exper,beauty$res,pch=19,col="blue")
+plot(beauty$female,beauty$res,pch=19,col="blue")
+plot(q7,which=1)
+par(mfrow=c(1,1))
+
 #Test for heteroskedasticity
 
-#Re-estimate q7 with FGLS
+beauty$fit <- predict(q7)
+beauty$sqr <- (beauty$res)^2
+bpreg = lm(sqr ~ looks + educ + exper + female, data = beauty)
+summary(bpreg)
+
+#computed the F-stat by pulling the R^2 and DF directly from the model
+#using summary
+beautyF <- (summary(bpreg)$r.squared/4)/((1-summary(bpreg)$r.squared)/summary(bpreg)$df[2])
 
 #Re-estimate q7 with robust standard errors
+
+q7rse <- vcovHC(q7)
+
+coeftest(q7,q7rse)
 
